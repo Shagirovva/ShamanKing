47de02946dc8a03c51127a3bfd95a618af3b6a7c
Shagirovva <Shagirova.akb@gmail.com>
2020-11-28 23:26:49 +0600

    Add Episode.cs,add Class1.cs

---
+++ domain/shamanking/Episode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace shamanking
+{
+    public class Episode
+    {
+        public int Id { get; }//идентификатор, чтобы не путать
+        public string Title { get;  }//название эпизода,возвращаемый
+        public Episode(int id, string title)//
+        {
+            Id = id;
+            Title = title; //проинициализировать
+        }
+    }
+}
